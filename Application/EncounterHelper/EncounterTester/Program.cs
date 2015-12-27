f4df9bbd73ebdeb90fbdfeab951dccbe7eafab29
Mathieu Gamache <mathg99@hotmail.com>
2015-12-26 20:20:42 -0500

    Add encounter

--- Application/EncounterHelper/EncounterTester/Program.cs
+++ Application/EncounterHelper/EncounterTester/Program.cs
@@ -9,16 +9,23 @@ namespace EncounterTester
         static void Main(string[] args)
         {
             // Goblin Hideout
-            var serializeMonsters = SimpleFileReader.ReadFile("GoblinAmbush.json");
-            //var serializeMonsters = SimpleFileReader.ReadFile("PrisoOtage.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("GoblinAmbush.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("PriseOtage.json");
             //var serializeMonsters = SimpleFileReader.ReadFile("Klarg.json");
 
             // Cragmaw
             //var serializeMonsters = SimpleFileReader.ReadFile("Cragmaw_Entrance.json");
             //var serializeMonsters = SimpleFileReader.ReadFile("Chapelle.json");
-            //var serializeMonsters = SimpleFileReader.ReadFile("Owlbear.json");
+            var serializeMonsters = SimpleFileReader.ReadFile("Owlbear.json");
             //var serializeMonsters = SimpleFileReader.ReadFile("KingGrol.json");
+
             // Wave Echo Cave
+            //var serializeMonsters = SimpleFileReader.ReadFile("MineTunnel.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("WizardQuarter.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("ForgeOfSpell.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("NorthBarrack.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("CollapsedCavern.json");
+            //var serializeMonsters = SimpleFileReader.ReadFile("TempleDomathoin.json");
 
             var monsters = JsonParserHelper.ParseList<Monster>(serializeMonsters);
 
